32583fcf49bad9efca3cf9207c977624cf2829bd
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-26 05:11:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,8 @@
-#def prime?(int)
-# if int > 1
-#    (2..int - 1).all? { |i| int % i != 0}
-#else
-#  false
-#  end
-#end
-
 def prime?(int)
-  #int.split(//)
-  int.all? do |i|
-    if i > 2 && i.even? && i % 3 != 0
-      false
-    else
-      true
-    end
+ if int > 1
+    (2..int - 1).all? { |i| int % i != 0}
+else
+  false
   end
 end
+
